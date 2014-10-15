0f591c1bcba421da666a5c3e1782be0667696740
Guido <guidopercu@gmail.com>
2014-10-15 15:04:41 -0300

    Segundo teste passando

--- encurtador/models.py
+++ encurtador/models.py
@@ -4,5 +4,12 @@ class Link(models.Model):
     url = models.URLField()
 
     @staticmethod
-    def shorten(url_original):
-        return "h"
+    def encurtar(link):
+        l, _ = Link.objects.get_or_create(url=link.url)
+        return str(l.pk)
+
+    @staticmethod
+    def expandir(slug):
+        link_id = int(slug)
+        l = Link.objects.get(pk=link_id)
+        return l.url
--- encurtador/tests.py
+++ encurtador/tests.py
@@ -2,11 +2,24 @@ from django.test import TestCase
 from .models import Link
 
 class ShortenerText(TestCase):
-    def test_shortens(self):
+    def test_encurtar(self):
         """
         Testa que a URL é encurtada
         """
         url = "http://www.google.com/"
         l = Link(url=url)
-        short_url = Link.shorten(l)
-        self.assertLess(len(short_url), len(url))
+        url_encurtada = Link.encurtar(l)
+        self.assertLess(len(url_encurtada), len(url))
+
+    def test_recuperar_link(self):
+        """
+        Testa que a URL encurtada e a URL original são a mesma
+        """
+        url_original = "http://www.google.com/"
+        l = Link(url=url_original)
+        url_encurtada = Link.encurtar(l)
+        l.save()
+        url_expandida = Link.expandir(url_encurtada)
+        self.assertEqual(url_original, url_expandida)
+
+
