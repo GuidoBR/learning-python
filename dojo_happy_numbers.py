75ee520bd84101f2dcf3327c87ef14e69229f3f3
Guido <guidopercu@gmail.com>
2015-07-01 18:02:35 -0300

    PEP8

--- dojo_happy_numbers.py
+++ dojo_happy_numbers.py
@@ -1,6 +1,7 @@
 # Henrique Bastos - Happy Numbers -  https://youtu.be/9gokU36gZTY
 def happy(number):
-    return number in (1, 7) if number < 10 else sum((int(char) ** 2) for char in str(number))
+    _next = sum((int(char) ** 2) for char in str(number))
+    return number in (1, 7) if number < 10 else _next
 
 assert all(happy(n) for n in (1, 10, 100, 97, 130))
 assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
