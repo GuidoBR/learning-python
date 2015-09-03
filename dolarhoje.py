268430f4e6ab87312074f39b30e71524e24ee4d4
Guido <guidopercu@gmail.com>
2015-09-03 18:09:13 -0300

    Notify Send do Ubuntu

--- dolarhoje.py
+++ dolarhoje.py
@@ -1,9 +1,10 @@
 import requests
+import subprocess
 from lxml import html
 
 url = "http://www.dolarhoje.com/"
 
 r = requests.get(url)
 dom = html.fromstring(r.text)
-dolar_em_real = dom.xpath("//*[@id='nacional']")
-print(dolar_em_real[0].value)
+dolar_em_real = dom.xpath("//*[@id='nacional']")[0].value
+subprocess.Popen(['notify-send', 'Dólar hoje vale: R$ %s' % str(dolar_em_real)])
