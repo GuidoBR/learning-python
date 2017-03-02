61c9580772f8ad24718995b587d740a0e4d2b768
Guido <guidopercu@gmail.com>
2017-03-02 09:12:05 -0300

    Começando a brincar com biopython

---
+++ bioinformatica/hello-world.py
@@ -0,0 +1,13 @@
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+sequencia = 'ACTCCACTG'
+
+s = Seq(sequencia, IUPAC.unambiguous_dna)
+complementar = s.complement()
+reverso_complementar = s.reverse_complement()
+
+rna = s.transcribe()
+proteina = s.translate()
+
+print("DNA: {} | RNA: {} | Proteina: {} \nComplemento: {} | Comlpemento Reverso: {}".format(s, rna, proteina, complementar, reverso_complementar))
