5b00753ffe600ee388ce1c9d50a341eb5bf0faa2
Guido Percú <guidopercu@gmail.com>
2022-10-28 09:48:34 -0300

    Bioinformatica

--- bioinformatica/1b.py
+++ bioinformatica/1b.py
@@ -1,13 +1,24 @@
-dna_string = input()
-reverse = dna_string[::-1]
-complemento = ''
-for letra in reverse:
-    if letra == 'A':
-        complemento += 'T'
-    elif letra == 'C':
-        complemento += 'G'
-    elif letra == 'G':
-        complemento += 'C'
-    elif letra == 'T':
-        complemento += 'A'
-print(complemento)
+import doctest
+
+
+def reverse_complement(dna_string):
+    """
+    Find the Reverse Complement of a String
+    https://rosalind.info/problems/ba1c/
+
+    Given: A DNA string Pattern.
+    Return: Pattern, the reverse complement of Pattern.
+    >>> reverse_complement('AAAACCCGGT')
+    'ACCGGGTTTT'
+    """
+    reverse = dna_string[::-1]
+    dna_translate = {'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C'}
+    complement = ''
+    for nucleotide in reverse:
+        complement += dna_translate[nucleotide]
+        
+    return complement
+
+
+if __name__ == '__main__':
+    doctest.testmod()
